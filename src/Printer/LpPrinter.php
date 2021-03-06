80527dc14921f4433d731c122bd43eac7a22f273
Nils Hodys <dev@pmail.nils.bz>
2021-03-06 22:04:52 +0100

    [master] Neue print darstellungsweise

--- src/Printer/LpPrinter.php
+++ src/Printer/LpPrinter.php
@@ -4,23 +4,41 @@ namespace Printer;
 
 class LpPrinter
 {
-    public function print(array $lp): void
+    public const PRINT_DIRECTION_FORWARD = 1;
+    public const PRINT_DIRECTION_REVERSE = 2;
+
+    public function print(array $lp, $printDirection = self::PRINT_DIRECTION_FORWARD): void
     {
         if (isset($lp['szf'])) {
-            echo $lp['szf']['typ'] . ' s = ' . $this->runden($lp['szf']['b']);
-            foreach ($lp['szf']['x'] as $variable => $koeff) {
-                echo ($koeff >= 0 ? ' +' : ' ') . $this->runden($koeff) . $variable;
-            }
-            echo PHP_EOL;
+            $this->printZf($lp['szf'], 's');
         }
 
-        echo $lp['zf']['typ'] . ' z = ' . $this->runden($lp['zf']['b']);
-        foreach ($lp['zf']['x'] as $variable => $koeff) {
+        $this->printZf($lp['zf']);
+
+        switch ($printDirection) {
+            case self::PRINT_DIRECTION_FORWARD:
+                $this->printStForward($lp['st']);
+                break;
+            case self::PRINT_DIRECTION_REVERSE:
+                $this->printStReverse($lp['st']);
+                break;
+        }
+
+        echo PHP_EOL;
+    }
+
+    private function printZf(array $zf, string $varName = 'z'): void
+    {
+        echo $zf['typ'] . ' ' . $varName . ' = ' . $this->runden($zf['b']);
+        foreach ($zf['x'] as $variable => $koeff) {
             echo ($koeff >= 0 ? ' +' : ' ') . $this->runden($koeff) . $variable;
         }
         echo PHP_EOL;
+    }
 
-        foreach ($lp['st'] as $restriktionszeile) {
+    private function printStForward(array $st): void
+    {
+        foreach ($st as $restriktionszeile) {
             $first = true;
             foreach ($restriktionszeile['x'] as $variable => $koeff) {
                 echo ($first ? '' : ($koeff >= 0 ? ' +' : ' ')) . $this->runden($koeff) . $variable;
@@ -28,20 +46,37 @@ class LpPrinter
             }
 
             if (isset($restriktionszeile['b'])) {
-                echo($restriktionszeile['b'] >= 0 ? ' +' . $this->runden($restriktionszeile['b']) : ' ' . $this->runden($restriktionszeile['b']));
+                echo ($restriktionszeile['b'] >= 0 ? ' +' : ' ') . $this->runden($restriktionszeile['b']);
             }
 
             echo ' ' . $restriktionszeile['operator'] . ' ' . $restriktionszeile['schranke'] . PHP_EOL;
         }
+    }
 
-        echo PHP_EOL;
+    private function printStReverse(array $st): void
+    {
+        foreach ($st as $restriktionszeile) {
+            echo $restriktionszeile['schranke'] . ' ' . $restriktionszeile['operator'];
+
+            if (isset($restriktionszeile['b'])) {
+                echo ' ' . $this->runden($restriktionszeile['b']);
+            } else {
+                echo ' 0';
+            }
+
+            foreach ($restriktionszeile['x'] as $variable => $koeff) {
+                echo ($koeff >= 0 ? ' +' : ' ') . $this->runden($koeff) . $variable;
+            }
+
+            echo PHP_EOL;
+        }
     }
 
     private function runden($zahl): string
     {
-        if(!is_numeric($zahl)) return $zahl;
+        if (!is_numeric($zahl)) return $zahl;
 
-        if ((int) $zahl == $zahl) return $zahl;
+        if ((int)$zahl == $zahl) return $zahl;
 
         return number_format(
             $zahl, // zu konvertierende zahl
